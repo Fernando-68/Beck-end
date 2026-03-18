3dff230bdeda0cbbd2c20722a0bea8649f316156
FERNANDO DE SÁ SOUZA <25203836@CFP127ALUNO.LOCAL>
2026-03-18 16:15:26 -0300

    Fizemos um Json dentro de um Array,funções que extraem o nome do produto, cor e marca; Também aprendemos a criamos filtros.

--- AULA04-02/modulo/array_json.js
+++ AULA04-02/modulo/array_json.js
@@ -183,9 +183,160 @@ const manipularDadosJson = function(){
     console.table(aluno)
 
     console.log(aluno.nome)
+    console.log(aluno.email)
+
+    aluno.telefone = "011-975845252"
+    aluno.data_nascimento = "13/03/2000"
+
+    console.log(aluno)
+
+    delete aluno.email
+    console.log(aluno.nome)
+
+    aluno.RA = 123456789
+    console.log(aluno)
+
+    aluno.nota = null
+    console.log(aluno)
 }
 
-manipularDadosJson()
+const cadastroDeProdutos = function(){
+    let cores = [
+        
+        {"id":1, "cor":"Branco", "Hexa": "#ffffff"}, //0
+        {"id":2, "cor":"Preto", "Hexa": "#00000"}, //1
+        {"id":3, "cor": "Azul", "Hexa": "#0000ff"}, //2
+        {"id":4, "cor": "Amarelo", "Hexa": "#ffff00"}, //3
+        {"id":5, "cor": "Rosa", "Hexa": "#ffb5c0"} //4
+
+
+    ]
+
+    let marcas = [ 
+        {"id":1, "marca": "Nvidida", "Telefone": "11 42378-1237", "Email":"nvidia@gmail"}, //0
+        {"id":2, "marca": "Dell", "Telefone": "11 12344-5678", "Email":"dell@gmail"}, //1
+        {"id":3, "arca": "Asus", "Telefone": "11 33624-6789", "Email":"asus@gmail"}, //2
+        {"id":4, "marca": "Intel", "Telefone": "11 12321-3232", "Email":"intel@gmail"}, //3
+        {"id":5, "marca": "MultiLaser", "Telefone": "11 45673-9087", "Email":"multilaser@gmail"}, //4
+        {"id":6, "marca": "Acer", "Telefone": "11 76456-8899", "Email":"acer@gmail"}, //5
+        {"id":7, "marca": "Rayzer", "Telefone": "11 14532-6745", "Email":"Nvidia@gmail"} //6
+    ]
+
+    let produtos = [
+        {   "id": 1,
+            "nome": "Monitor",
+            "descrição": "Monitor de 27 Polegadas",
+            "valor": 1500,
+            "qtde": 20,
+            "cor": [
+                cores[0],
+                cores[1]
+            ],
+            "marca": [
+                marcas[1].marca
+            ]
+        },
+        {
+            "id": 2,
+            "nome": "Teclado",
+            "descrição": "Teclado Mecânico RGB",
+            "valor": 250,
+            "qtde": 500,
+            "cor": cores,
+            "marca": [
+                marcas[4].marca,
+                marcas[6].marca
+            ]
+        },
+        {
+            "id": 3,
+            "nome": "Mouse",
+            "descrição": "Mouse sem fio",
+            "valor": 80,
+            "qtde": 160,
+            "cor": [
+                cores[1],
+                cores[3],
+                cores[4]
+
+            ],
+            "marca": [
+                marcas[1].marca,
+                marcas[4].marca,
+                marcas[6].marca
+            ]
+        }
+    ]
+    console.log(produtos)
+    console.table(produtos)
+    console.log(produtos[0].Cor)
+
+    //Exibindo as cores referente ao produto "MONITOR"
+    produtos[0].cor.forEach(function(itemCor){
+        console.log(itemCor.cor)
+    })
+
+    console.log(produtos)
+    console.table(produtos)
+
+    //permite extarir os produtos
+    produtos.forEach(function(itemProduto){
+        console.log(`Produto: ${itemProduto.nome}`)
+
+        //Permite extrair as cores dentro de cada produto
+        itemProduto.cor.forEach(function(itemCor){
+            console.log(`   Cor: ${itemCor.cor}`)
+        })
+
+            //Permite extrair as marcas dentro de cada produto
+            itemProduto.marca.forEach(function(itemMarca){
+                console.log(`   Marca: ${itemMarca}`)
+            }) 
+                
+    })
+       
+     //Filtrando produtos pelo nome
+     console.log('Exemplo de como pesquisar um produto pelo nome')
+
+     let nomeProduto = 'Mouse'
+     produtos.forEach(function(itemProduto){
+        if(String(nomeProduto).toUpperCase == String(itemProduto.nome).toUpperCase){
+            console.log(itemProduto)
+        }
+     })
+
+     console.log('Exemplo de como pesquisar um produto pela cor do produto')
+
+     let nomeCor = 'rosa'
+
+     produtos.forEach(function(itemProduto){
+       itemProduto.cor.forEach(function(itemCor){
+        if(String(nomeCor).toUpperCase() == String(itemCor).toUpperCase()){
+            console.log(itemProduto
+            
+            )
+        }
+       })
+       
+        })
+     
+
+    
+   
+    
+
+    //console.log(cores[2].Cor)
+
+    //for(let contador = 0; contador < cores.length; contador++){
+       // let item = cores[contador]
+       // console.log(`O nome da cor: ${item.Cor}`)
+  //  }
+
+}
+
+
+cadastroDeProdutos()
+//manipularDadosJson()
 //console.log(verificaritem('Maria'))
 //console.table(listaDeAlunos)
 //removerItem('Maria')
